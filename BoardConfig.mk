eb4ffce6d93292772072955ed64f36ac44acebdc
星纪Lin <109717106+xingjilin08@users.noreply.github.com>
2026-08-03 03:15:23 +0800

    更新 BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -90,7 +90,7 @@ TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
 TW_INCLUDE_REPACKTOOLS := true
-TW_MAX_BRIGHTNESS := 2047
+TW_MAX_BRIGHTNESS := 16383
 
 # 添加这行代码来加载 OrangeFoxConfig.mk
 -include $(DEVICE_PATH)/OrangeFoxConfig.mk
